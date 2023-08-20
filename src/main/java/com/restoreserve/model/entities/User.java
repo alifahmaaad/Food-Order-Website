bd544d6d01a35b2286568de36e9f0b5cf57dd751
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-20 10:16:31 +0700

    add user and role entity

---
+++ src/main/java/com/restoreserve/model/entities/User.java
@@ -0,0 +1,29 @@
+package com.restoreserve.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Role role;
+
+}
