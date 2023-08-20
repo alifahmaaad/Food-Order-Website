5ed4432528c3fff5569b3b520580a0060698a636
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-20 13:48:59 +0700

    add reservation service

--- src/main/java/com/restoreserve/model/repos/ReservationRepo.java
+++ src/main/java/com/restoreserve/model/repos/ReservationRepo.java
@@ -1,9 +1,13 @@
 package com.restoreserve.model.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.restoreserve.model.entities.Reservation;
 
 public interface ReservationRepo extends JpaRepository<Reservation, Long>{
-    
+    //derived query JPA
+    List<Reservation> findbyIdUser(Long id_user);
+    List<Reservation> findbyIdRestaurant(Long id_restaurant);   
 }
