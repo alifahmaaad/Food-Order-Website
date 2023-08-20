7343d72e9b9e5966ae743d783c58bf8214e56523
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-20 18:50:52 +0700

    add registeruserdto and responsedata

---
+++ src/main/java/com/restoreserve/dto/RegisterUserDto.java
@@ -0,0 +1,25 @@
+package com.restoreserve.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterUserDto {
+    
+    @NotEmpty(message = "Fullname is Required")
+    public String fullName;
+    @NotEmpty(message = "Username is Required")
+    public String username;
+    @NotEmpty(message = "Email is Required")
+    @Email
+    public String email;
+    @NotEmpty(message = "Password is Required")
+    public String password;
+    @NotEmpty(message = "Role is Required")
+    public String role;
+}
--- src/main/java/com/restoreserve/model/entities/User.java
+++ src/main/java/com/restoreserve/model/entities/User.java
@@ -24,6 +24,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String fullName;
     @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false, unique = true)
