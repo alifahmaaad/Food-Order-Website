cec8a5af41e1149f8795aeb5365b8a4ba69580ee
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-20 19:01:40 +0700

    add endpoint getuserbyid in usercontroller

--- src/main/java/com/restoreserve/controlers/UserController.java
+++ src/main/java/com/restoreserve/controlers/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +28,7 @@ public class UserController {
     private UserService userService;
     @PostMapping("/register")
     public ResponseEntity<ResponseData<User>> register(@Valid @RequestBody RegisterUserDto userDto,Errors errs){
-        ResponseData<User> dataResponse = new ResponseData<>();
-        dataResponse.setPayload(null);
-        dataResponse.setStatus(false);
+        ResponseData<User> dataResponse = new ResponseData<User>(false, null, null);
         if(errs.hasErrors()){
             for (ObjectError err : errs.getAllErrors()) {
             dataResponse.getMessage().add(err.getDefaultMessage());
@@ -38,10 +38,29 @@ public class UserController {
         User dataUser = modelMapper.map(userDto, User.class);
         try {
             dataResponse.setPayload(userService.create(dataUser));
+            dataResponse.setStatus(true);
             dataResponse.getMessage().add("Your Account has been succesfully created or registered");
             return ResponseEntity.ok(dataResponse);
         } catch (Exception e) {
             dataResponse.getMessage().add(e.getMessage());
+            return ResponseEntity.badRequest().body(dataResponse);
+        }
+    }
+    @GetMapping("/user/{id}")
+    public ResponseEntity<ResponseData<User>> getUserById(@PathVariable Long id){
+        ResponseData<User> dataResponse=new ResponseData<User>(false, null, null);
+        try {
+            boolean isExists = userService.isUserExists(id);
+            if(isExists){
+                dataResponse.setPayload(userService.getUserById(id));
+                dataResponse.getMessage().add("Success get data user with id :"+id);
+                dataResponse.setStatus(true);
+                return ResponseEntity.ok(dataResponse);
+            }
+            dataResponse.getMessage().add("Failed: Data user with id :"+id+" Not Found");
+            return ResponseEntity.badRequest().body(dataResponse);
+        } catch (Exception e) {
+             dataResponse.getMessage().add(e.getMessage());
             return ResponseEntity.badRequest().body(dataResponse);
         }
     }
