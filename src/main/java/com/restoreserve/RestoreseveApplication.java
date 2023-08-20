e4acd5da87283dbdfb947d757ac21dd28c0b4b60
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-20 10:03:14 +0700

    initial commit

---
+++ src/main/java/com/restoreserve/RestoreseveApplication.java
@@ -0,0 +1,13 @@
+package com.restoreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoreseveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoreseveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restoreserve/restoreseve/RestoreseveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restoreserve.restoreseve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoreseveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
