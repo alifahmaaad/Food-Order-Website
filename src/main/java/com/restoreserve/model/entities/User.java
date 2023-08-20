e945e2c2b2b6329f570ea949c8110df75fc485ee
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-20 13:48:06 +0700

    delete role entity and repo, repo field will be in user entity

--- src/main/java/com/restoreserve/model/entities/User.java
+++ src/main/java/com/restoreserve/model/entities/User.java
@@ -1,12 +1,15 @@
 package com.restoreserve.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.restoreserve.enums.RoleEnum;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,11 +24,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String username;
+    @Column(nullable = false, unique = true)
     private String email;
+    @JsonIgnore
     private String password;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_role")
-    private Role role;
+    @Enumerated(EnumType.STRING)
+    private RoleEnum role;
 
 }
--- src/main/java/com/restoreserve/model/repos/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.restoreserve.model.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.restoreserve.model.entities.Role;
-
-public interface RoleRepo extends JpaRepository<Role, Long>{
-    
-}
