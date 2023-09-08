bf15971fbb5063f3bd37aa82fd00eac974613855
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-09-08 14:40:05 +0700

    fix controller update and create

--- src/main/java/com/restoreserve/controlers/MenuController.java
+++ src/main/java/com/restoreserve/controlers/MenuController.java
@@ -9,10 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +24,7 @@ import com.restoreserve.model.entities.Menu;
 import com.restoreserve.model.entities.Restaurant;
 import com.restoreserve.model.entities.User;
 import com.restoreserve.security.ImplementUserDetails.CustomUserDetails;
+import com.restoreserve.services.ImageService;
 import com.restoreserve.services.MenuService;
 import com.restoreserve.services.RestaurantService;
 import com.restoreserve.services.UserService;
@@ -41,23 +42,36 @@ public class MenuController {
     private UserService userService;
     @Autowired
     private ModelMapper modelMapper;
-    //restaurant, when id_resto on menu same as id user in auth
+    @Autowired
+    private ImageService imageService;
+
+    // restaurant, when id_resto on menu same as id user in auth
     @PostMapping("/restaurant/create")
-    public ResponseEntity<ResponseData<Menu>> create(@Valid @RequestBody CreateMenuDto menuDto, @AuthenticationPrincipal CustomUserDetails userDetails){
+    public ResponseEntity<ResponseData<Menu>> create(@Valid @ModelAttribute CreateMenuDto menuDto,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        System.out.println("masukkkk bang");
         ResponseData<Menu> dataResponse = new ResponseData<>(false, new ArrayList<>(), null);
         try {
             Restaurant dataRestaurant = restaurantService.getRestaurantById(menuDto.getRestaurant());
-            if(dataRestaurant!=null){
-                 if(isUserAllowedToAccessThisEndpoint(userDetails)||dataRestaurant.getUserOwner().getId().equals(userDetails.getId())){ 
-                     //need to check cause only user it self can update except his role superadmin or appadmin
-                     Menu menu = modelMapper.map(menuDto, Menu.class);
-                     menu.setRestaurant(dataRestaurant);
-                     dataResponse.setPayload(menuService.create(menu));
-                     dataResponse.getMessage().add("Success Add Menu");
-                     dataResponse.setStatus(true);
-                     return ResponseEntity.ok(dataResponse);
+            if (dataRestaurant != null) {
+                if (isUserAllowedToAccessThisEndpoint(userDetails)
+                        || dataRestaurant.getUserOwner().getId().equals(userDetails.getId())) {
+                    // need to check cause only user it self can update except his role superadmin
+                    // or appadmin
+                    Menu menu = modelMapper.map(menuDto, Menu.class);
+                    menu.setRestaurant(dataRestaurant);
+                    if (!menuDto.getPhoto().isEmpty()) {
+                        String imagePath = imageService.saveImage(menuDto.getPhoto(), "menu");
+                        menu.setPhoto(imagePath);
+                    } else {
+                        menu.setPhoto("uploads/menu/default.png");
+                    }
+                    dataResponse.setPayload(menuService.create(menu));
+                    dataResponse.getMessage().add("Success Add Menu");
+                    dataResponse.setStatus(true);
+                    return ResponseEntity.ok(dataResponse);
                 }
-                dataResponse.getMessage().add("You are not authorized to Create menu in this resataurant");
+                dataResponse.getMessage().add("You are not authorized to Create menu in this restaurant");
                 return ResponseEntity.badRequest().body(dataResponse);
             }
             dataResponse.getMessage().add("Restaurant not found");
@@ -67,9 +81,10 @@ public class MenuController {
             return ResponseEntity.badRequest().body(dataResponse);
         }
     }
-    //all
+
+    // all
     @GetMapping("/all/{id}")
-    public ResponseEntity<ResponseData<List<Menu>>> getAllMenuByIdRestaurant(@PathVariable Long id){
+    public ResponseEntity<ResponseData<List<Menu>>> getAllMenuByIdRestaurant(@PathVariable Long id) {
         ResponseData<List<Menu>> dataResponse = new ResponseData<>(false, new ArrayList<>(), null);
         try {
             dataResponse.setPayload(menuService.getMenuByRestaurantId(id));
@@ -77,13 +92,14 @@ public class MenuController {
             dataResponse.setStatus(true);
             return ResponseEntity.ok(dataResponse);
         } catch (Exception e) {
-             dataResponse.getMessage().add(e.getMessage());
+            dataResponse.getMessage().add(e.getMessage());
             return ResponseEntity.badRequest().body(dataResponse);
         }
     }
-    //all
+
+    // all
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseData<Menu>> getMenuById(@PathVariable Long id){
+    public ResponseEntity<ResponseData<Menu>> getMenuById(@PathVariable Long id) {
         ResponseData<Menu> dataResponse = new ResponseData<>(false, new ArrayList<>(), null);
         try {
             dataResponse.setPayload(menuService.getMenuById(id));
@@ -91,25 +107,37 @@ public class MenuController {
             dataResponse.setStatus(true);
             return ResponseEntity.ok(dataResponse);
         } catch (Exception e) {
-             dataResponse.getMessage().add(e.getMessage());
+            dataResponse.getMessage().add(e.getMessage());
             return ResponseEntity.badRequest().body(dataResponse);
         }
     }
-    //restaurant, when id_resto on menu same as id user in auth
+
+    // restaurant, when id_resto on menu same as id user in auth
     @PutMapping("/restaurant/update")
-    public ResponseEntity<ResponseData<Menu>> update(@Valid @RequestBody UpdateMenuDto menuDto, @AuthenticationPrincipal CustomUserDetails userDetails){
+    public ResponseEntity<ResponseData<Menu>> update(@Valid @ModelAttribute UpdateMenuDto menuDto,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
         ResponseData<Menu> dataResponse = new ResponseData<>(false, new ArrayList<>(), null);
         try {
             Restaurant dataRestaurant = restaurantService.getRestaurantById(menuDto.getRestaurant());
-            if(dataRestaurant!=null){
-                if(isUserAllowedToAccessThisEndpoint(userDetails)||dataRestaurant.getUserOwner().getId().equals(userDetails.getId())){ 
-                     //need to check cause only user it self can update except his role superadmin or appadmin
-                     Menu dataMenu = modelMapper.map(menuDto, Menu.class);
-                     dataMenu.setRestaurant(dataRestaurant);
-                     dataResponse.setPayload(menuService.update(dataMenu));
-                     dataResponse.getMessage().add("Success Update Menu");
-                     dataResponse.setStatus(true);
-                     return ResponseEntity.ok(dataResponse);
+            if (dataRestaurant != null) {
+                if (isUserAllowedToAccessThisEndpoint(userDetails)
+                        || dataRestaurant.getUserOwner().getId().equals(userDetails.getId())) {
+                    // need to check cause only user it self can update except his role superadmin
+                    // or appadmin
+                    Menu menuPrev = menuService.getMenuById(menuDto.getId());
+                    Menu dataMenu = modelMapper.map(menuDto, Menu.class);
+                    dataMenu.setRestaurant(dataRestaurant);
+                    if (!menuDto.getPhoto().isEmpty()) {
+                        imageService.deleteImage(menuPrev.getPhoto());
+                        String imagePath = imageService.saveImage(menuDto.getPhoto(), "menu");
+                        dataMenu.setPhoto(imagePath);
+                    } else {
+                        dataMenu.setPhoto(menuPrev.getPhoto());
+                    }
+                    dataResponse.setPayload(menuService.update(dataMenu));
+                    dataResponse.getMessage().add("Success Update Menu");
+                    dataResponse.setStatus(true);
+                    return ResponseEntity.ok(dataResponse);
                 }
                 dataResponse.getMessage().add("You are not authorized to Update menu in this resataurant");
                 return ResponseEntity.badRequest().body(dataResponse);
@@ -121,15 +149,20 @@ public class MenuController {
             return ResponseEntity.badRequest().body(dataResponse);
         }
     }
-    //restaurant, when id_resto on menu same as id user in auth
+
+    // restaurant, when id_resto on menu same as id user in auth
     @DeleteMapping("/restaurant/delete/{id}")
-    public ResponseEntity<ResponseData<?>> delete(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails){
-        ResponseData<?> dataResponse= new ResponseData<>(false, new ArrayList<>(), null);
+    public ResponseEntity<ResponseData<?>> delete(@PathVariable Long id,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        ResponseData<?> dataResponse = new ResponseData<>(false, new ArrayList<>(), null);
         try {
-            if(menuService.isExist(id)){
-                Restaurant dataRestaurant = restaurantService.getRestaurantById(menuService.getMenuById(id).getRestaurant().getId());
-                if(isUserAllowedToAccessThisEndpoint(userDetails)||dataRestaurant.getUserOwner().getId().equals(userDetails.getId())){ 
-                    //need to check cause only user it self can update except his role superadmin or appadmin
+            if (menuService.isExist(id)) {
+                Restaurant dataRestaurant = restaurantService
+                        .getRestaurantById(menuService.getMenuById(id).getRestaurant().getId());
+                if (isUserAllowedToAccessThisEndpoint(userDetails)
+                        || dataRestaurant.getUserOwner().getId().equals(userDetails.getId())) {
+                    // need to check cause only user it self can update except his role superadmin
+                    // or appadmin
                     menuService.delete(id);
                     dataResponse.setStatus(true);
                     dataResponse.getMessage().add("success delete menu");
@@ -148,6 +181,6 @@ public class MenuController {
 
     private boolean isUserAllowedToAccessThisEndpoint(CustomUserDetails userDetails) {
         User userData = userService.findByUsername(userDetails.getUsername());
-        return userData.getRole().equals(RoleEnum.Super_Admin)||userData.getRole().equals(RoleEnum.App_Admin);
+        return userData.getRole().equals(RoleEnum.Super_Admin) || userData.getRole().equals(RoleEnum.App_Admin);
     }
 }
